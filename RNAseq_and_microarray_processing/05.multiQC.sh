280849006976edb86e153d8d1b525df6cca184c0
Joel Martinez-Miralles <marmirjoel@gmail.com>
2025-09-12 11:51:09 +0200

    Update 05.multiQC.sh

--- RNAseq_and_microarray_processing/05.multiQC.sh
+++ RNAseq_and_microarray_processing/05.multiQC.sh
@@ -15,7 +15,9 @@ multiqc --version
 # Set project path
 root_dir="/path/to/the/project"
 
-# 
+# Set input and output directories
+input_dir="$root_dir/data/02_preprocessed_fastqs/00_temp/"
+output_dir="$root_dir/data/02_preprocessed_fastqs/00_temp/z_multiqc_report"
 
 # Run multiqc to gather qc metrics
-multiqc "$root_dir/data/02_preprocessed_fastqs/00_temp/" -o "$root_dir/data/02_preprocessed_fastqs/00_temp/z_multiqc_report"
+multiqc $input_dir -o $output_dir
