02da8f5d48c3c31bd0d404af2e35d81711d79793
Xabier de Martin <fxabierd@gmail.com>
2026-02-16 17:26:53 +0100

    Add files via upload

---
+++ data_processing/RNAseq_and_microarray_processing/05.multiQC.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#############################################################
+###### Summarize quality checks after fastp processing ######
+#############################################################
+
+# -----------------
+# -- Use MultiQC --
+# -----------------
+
+# Load Modules
+module load Miniconda3/20240927
+multiqc --version
+
+# Set project path
+root_dir="/path/to/the/project"
+
+# Set input and output directories
+input_dir="$root_dir/data/02_preprocessed_fastqs/00_temp/"
+output_dir="$root_dir/data/02_preprocessed_fastqs/00_temp/z_multiqc_report"
+
+# Run multiqc to gather qc metrics
+multiqc "$input_dir" -o "$output_dir"
