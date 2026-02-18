26921494b3bc13915e5947c6a9d083c0800c642e
Xabier de Martin <fxabierd@gmail.com>
2026-02-18 17:36:05 +0100

    Fix file names in ATAC-seq data processing script

--- data_processing/epigenomic_data/fold_change_chromatin_data_processing.sh
+++ data_processing/epigenomic_data/fold_change_chromatin_data_processing.sh
@@ -5,12 +5,12 @@
 ###### ATAC-seq
 
 # Mouse
-for file in aydin_0h_atac_1.sam.processed.bam aydin_12h_ascl1_atac_rep1.sam.processed.bam aydin_neurog2_12h_atac_rep1.sam.processed.bam \
+for file in aydin_0h_atac_1.sam.processed.bam aydin_12h_ascl1_atac.sam.processed.bam aydin_neurog2_12h_atac.sam.processed.bam \
 casey_esc_uninduced_ascl1_atac.fastq.gz.sam.processed.bam casey_24h_ascl1_atac.fastq.gz.sam.processed.bam \
 casey_esc_uninduced_ascl2_atac.fastq.gz.sam.processed.bam casey_24h_ascl2_atac.fastq.gz.sam.processed.bam \
 casey_esc_uninduced_myod1_atac.fastq.gz.sam.processed.bam casey_24h_myod1_atac.fastq.gz.sam.processed.bam \
-lee_mef_atac.sam.processed.bam lee_ascl1_48h_atac_rep1.sam.processed.bam lee_myod1_48h_atac_rep1.sam.processed.bam \
-wapinski_MEF.sam.processed.bam wapinski_MEF+Ascl1_48hr_B1_rep2.sam.processed.bam
+lee_mef_atac.sam.processed.bam lee_ascl1_48h_atac.sam.processed.bam lee_myod1_48h_atac.sam.processed.bam \
+wapinski_MEF.sam.processed.bam wapinski_MEF+Ascl1_48hr_B1.sam.processed.bam
 do
   sbatch --partition=bigmem --wrap="macs2 callpeak -f BAM -t $file -g mm --outdir . -n $file"
 done
@@ -19,7 +19,7 @@ done
 
 # faltan
 cd ~/proneural/mouse/pairedd
-for file in pereira_astrocytes_GSM6368722_ATACseq_GFP_rep1.sam.rmdup.bam pereira_astrocytes_GSM6368725_ATACseq_Ngn2_rep1.sam.rmdup.bam
+for file in pereira_astrocytes_GSM6368722_ATACseq_GFP.sam.rmdup.bam pereira_astrocytes_GSM6368725_ATACseq_Ngn2.sam.rmdup.bam
 do
   sbatch --partition=bigmem --mem=400000 --wrap="macs2 callpeak -f BAM -t $file -g mm --outdir . -n $file"
 done
@@ -48,13 +48,13 @@ done
 
 # Human
 cd ~/proneural
-for file in park_atac_control_rep1.fastq.gz.sam.processed.bam park_atac_dox_rep1.fastq.sam.processed.bam
+for file in park_atac_control.fastq.gz.sam.processed.bam park_atac_dox.fastq.sam.processed.bam
 do
   sbatch --partition=bigmem --mem=400000 --wrap="macs2 callpeak -f BAM -t $file -g hs --outdir . -n $file"
 done
 
 cd ~/proneural/human/paired
-for file in wang_ATAC_GIMEN_ASCL1_Neg_Rep1.sam.processed.bam wang_ATAC_GIMEN_ASCL1_Pos_Rep1.sam.processed.bam
+for file in wang_ATAC_GIMEN_ASCL1_Neg.sam.processed.bam wang_ATAC_GIMEN_ASCL1_Pos.sam.processed.bam
 do
   sbatch --partition=bigmem --mem=400000 --wrap="macs2 callpeak -f BAM -t $file -g hs --outdir . -n $file"
 done
@@ -84,7 +84,7 @@ do
   sbatch --partition=bigmem --mem=400000 --wrap="macs2 callpeak --broad -f BAM -t $file -g hs --outdir . -n $file"
 done
 
-for file in *manandhar_GSM2449222_Fb_rep1_dnase*bam *manandhar_GSM2449225_Fd_rep1_dnase*bam
+for file in *manandhar_GSM2449222_Fb_dnase*bam *manandhar_GSM2449225_Fd_dnase*bam
 do
   sbatch --partition=bigmem --mem=400000 --wrap="macs2 callpeak -f BAM -t $file -g hs --outdir . -n $file"
 done
@@ -107,18 +107,18 @@ concatenate_beds () {
   cat $1 ~/proneural/missing.bed | sort -k1,1 -k2,2n | grep -v JH | grep -v GL | grep -v random | grep -v chrM | grep -v KI | grep -v alt | uniq  > ~/proneural/$3.bed
 }
 
-concatenate_beds aydin_0h_atac_1.sam.processed.bam_peaks.narrowPeak aydin_12h_ascl1_atac_rep1.sam.processed.bam_peaks.narrowPeak "aydin_ascl1_12h_join"
-concatenate_beds aydin_0h_atac_1.sam.processed.bam_peaks.narrowPeak aydin_48h_neurog2_atac_rep1.sam.processed.bam_peaks.narrowPeak "aydin_neurog2_48h_join"
+concatenate_beds aydin_0h_atac_1.sam.processed.bam_peaks.narrowPeak aydin_12h_ascl1_atac.sam.processed.bam_peaks.narrowPeak "aydin_ascl1_12h_join"
+concatenate_beds aydin_0h_atac_1.sam.processed.bam_peaks.narrowPeak aydin_48h_neurog2_atac.sam.processed.bam_peaks.narrowPeak "aydin_neurog2_48h_join"
 concatenate_beds casey_esc_uninduced_ascl1_atac.fastq.gz.sam.processed.bam_peaks.narrowPeak casey_24h_ascl1_atac.fastq.gz.sam.processed.bam_peaks.narrowPeak "casey_ascl1_24h_join"
 concatenate_beds casey_esc_uninduced_ascl2_atac.fastq.gz.sam.processed.bam_peaks.narrowPeak casey_24h_ascl2_atac.fastq.gz.sam.processed.bam_peaks.narrowPeak "casey_ascl2_24h_join"
 concatenate_beds casey_esc_uninduced_myod1_atac.fastq.gz.sam.processed.bam_peaks.narrowPeak casey_24h_myod1_atac.fastq.gz.sam.processed.bam_peaks.narrowPeak "casey_myod1_24h_join"
-concatenate_beds lee_mef_atac.sam.processed.bam_peaks.narrowPeak lee_myod1_48h_atac_rep1.sam.processed.bam_peaks.narrowPeak "lee_myod1_48h_join"
-concatenate_beds lee_mef_atac.sam.processed.bam_peaks.narrowPeak lee_ascl1_48h_atac_rep1.sam.processed.bam_peaks.narrowPeak "lee_ascl1_48h_join"
-concatenate_beds wapinski_MEF.sam.processed.bam_peaks.narrowPeak wapinski_MEF+Ascl1_48hr_B1_rep1.sam.processed.bam_peaks.narrowPeak "wapinski_ascl1_48hr_join"
-concatenate_beds park_atac_control_rep1.fastq.gz.sam.processed.bam_peaks.narrowPeak park_atac_dox_rep1.fastq.sam.processed.bam_peaks.narrowPeak "park_ascl1_dox_join"
-concatenate_beds pereira_astrocytes_GSM6368722_ATACseq_GFP_rep1.sam.rmdup.bam_peaks.narrowPeak pereira_astrocytes_GSM6368725_ATACseq_Ngn2_rep1.sam.rmdup.bam_peaks.narrowPeak "pereira_astrocytes_ngn2_join"
-concatenate_beds wang_ATAC_GIMEN_ASCL1_Neg_Rep1.sam.processed.bam_peaks.narrowPeak wang_ATAC_GIMEN_ASCL1_Pos_Rep1.sam.processed.bam_peaks.narrowPeak "wang_ascl1_join"
-concatenate_beds manandhar_GSM2449222_Fb_rep1_dnase.fastq.gz.sam.rmdup.sam.processed.bam_peaks.narrowPeak manandhar_GSM2449225_Fd_rep1_dnase.fastq.gz.sam.processed.bam_peaks.narrowPeak "manandhar_myod1_join"
+concatenate_beds lee_mef_atac.sam.processed.bam_peaks.narrowPeak lee_myod1_48h_atac.sam.processed.bam_peaks.narrowPeak "lee_myod1_48h_join"
+concatenate_beds lee_mef_atac.sam.processed.bam_peaks.narrowPeak lee_ascl1_48h_atac.sam.processed.bam_peaks.narrowPeak "lee_ascl1_48h_join"
+concatenate_beds wapinski_MEF.sam.processed.bam_peaks.narrowPeak wapinski_MEF+Ascl1_48hr_B1.sam.processed.bam_peaks.narrowPeak "wapinski_ascl1_48hr_join"
+concatenate_beds park_atac_control.fastq.gz.sam.processed.bam_peaks.narrowPeak park_atac_dox.fastq.sam.processed.bam_peaks.narrowPeak "park_ascl1_dox_join"
+concatenate_beds pereira_astrocytes_GSM6368722_ATACseq_GFP.sam.rmdup.bam_peaks.narrowPeak pereira_astrocytes_GSM6368725_ATACseq_Ngn2.sam.rmdup.bam_peaks.narrowPeak "pereira_astrocytes_ngn2_join"
+concatenate_beds wang_ATAC_GIMEN_ASCL1_Neg.sam.processed.bam_peaks.narrowPeak wang_ATAC_GIMEN_ASCL1_Pos.sam.processed.bam_peaks.narrowPeak "wang_ascl1_join"
+concatenate_beds manandhar_GSM2449222_Fb_dnase.fastq.gz.sam.rmdup.sam.processed.bam_peaks.narrowPeak manandhar_GSM2449225_Fd_dnase.fastq.gz.sam.processed.bam_peaks.narrowPeak "manandhar_myod1_join"
 concatenate_beds li_GFP-DM_K27ac_1.fastq.gz.sam.processed.bam_peaks.broadPeak li_Tw2-DM_K27ac_1.fastq.gz.sam.processed.bam_peaks.broadPeak "li_tw2_induction_join"
 concatenate_beds fong_p19_control_ach4.fastq.gz.sam.processed.bam_peaks.broadPeak fong_p19_neurod2_induction_ach4.fastq.gz.sam.processed.bam_peaks.broadPeak "fong_neurod2_induction_join"
 concatenate_beds smith_IP1_H3K27AC_MRC5_1DPT.fastq.gz.sam.processed.bam_peaks.broadPeak smith_IP1_H3K27AC_N_1DPT.fastq.gz.sam.processed.bam_peaks.broadPeak "smith_neurog2_induction_join"
@@ -163,15 +163,15 @@ module load BEDTools
 
 
 
-bedmap_chromatin aydin_ascl1_12h_join.bed clean.aydin_0h_atac_2.sam.processed.bam.bdg clean.aydin_12h_ascl1_atac_rep2.sam.processed.bam.bdg
-bedmap_chromatin aydin_neurog2_48h_join.bed clean.aydin_0h_atac_2.sam.processed.bam.bdg clean.aydin_48h_neurog2_atac_rep2.sam.processed.bam.bdg
+bedmap_chromatin aydin_ascl1_12h_join.bed clean.aydin_0h_atac_2.sam.processed.bam.bdg clean.aydin_12h_ascl1_atac.sam.processed.bam.bdg
+bedmap_chromatin aydin_neurog2_48h_join.bed clean.aydin_0h_atac_2.sam.processed.bam.bdg clean.aydin_48h_neurog2_atac.sam.processed.bam.bdg
 bedmap_chromatin casey_ascl1_24h_join.bed clean.casey_esc_uninduced_ascl1_atac.fastq.gz.sam.processed.bam.bdg clean.casey_24h_ascl1_atac.fastq.gz.sam.processed.bam.bdg
 bedmap_chromatin casey_ascl2_24h_join.bed clean.casey_esc_uninduced_ascl2_atac.fastq.gz.sam.processed.bam.bdg clean.casey_24h_ascl2_atac.fastq.gz.sam.processed.bam.bdg
 bedmap_chromatin casey_myod1_24h_join.bed clean.casey_esc_uninduced_myod1_atac.fastq.gz.sam.processed.bam.bdg clean.casey_24h_myod1_atac.fastq.gz.sam.processed.bam.bdg
-bedmap_chromatin lee_myod1_48h_join.bed clean.lee_mef_atac.sam.processed.bam.bdg clean.lee_myod1_48h_atac_rep1.sam.processed.bam.bdg
-bedmap_chromatin lee_ascl1_48h_join.bed clean.lee_mef_atac.sam.processed.bam.bdg clean.lee_ascl1_48h_atac_rep1.sam.processed.bam.bdg
-bedmap_chromatin wapinski_ascl1_48hr_join.bed clean.wapinski_MEF.sam.processed.bam.bdg clean.wapinski_MEF+Ascl1_48hr_B2_rep1.sam.processed.bam.bdg
-bedmap_chromatin park_ascl1_dox_join.bed clean.park_atac_control_rep1.fastq.gz.sam.processed.bam.bdg clean.park_atac_dox_rep1.fastq.sam.processed.bam.bdg
+bedmap_chromatin lee_myod1_48h_join.bed clean.lee_mef_atac.sam.processed.bam.bdg clean.lee_myod1_48h_atac.sam.processed.bam.bdg
+bedmap_chromatin lee_ascl1_48h_join.bed clean.lee_mef_atac.sam.processed.bam.bdg clean.lee_ascl1_48h_atac.sam.processed.bam.bdg
+bedmap_chromatin wapinski_ascl1_48hr_join.bed clean.wapinski_MEF.sam.processed.bam.bdg clean.wapinski_MEF+Ascl1_48hr_B2.sam.processed.bam.bdg
+bedmap_chromatin park_ascl1_dox_join.bed clean.park_atac_control.fastq.gz.sam.processed.bam.bdg clean.park_atac_dox.fastq.sam.processed.bam.bdg
 bedmap_chromatin li_tw2_induction_join.bed clean.li_GFP-DM_K27ac_1.fastq.gz.sam.processed.bam.bdg clean.li_Tw2-DM_K27ac_1.fastq.gz.sam.processed.bam.bdg
 bedmap_chromatin smith_neurog2_induction_join.bed clean.smith_IP1_H3K27AC_MRC5_1DPT.fastq.gz.sam.processed.bam.bdg clean.smith_IP1_H3K27AC_N_1DPT.fastq.gz.sam.processed.bam.bdg
 bedmap_chromatin lin_myc_t24_join.bed clean.lin_P493-6_T0_H3K27AC.fastq.gz.sam.processed.bam.bdg clean.lin_P493-6_T24_H3K27AC.fastq.gz.sam.processed.bam.bdg
@@ -179,9 +179,9 @@ bedmap_chromatin zeid_mycn_6hr_join.bed clean.zeid_SHEP_0HR_H3K27AC.fastq.gz.sam
 bedmap_chromatin fong_neurod2_induction_join.bed clean.fong_p19_control_ach4.fastq.gz.sam.processed.bam.bdg clean.fong_p19_neurod2_induction_ach4.fastq.gz.sam.processed.bam.bdg
 bedmap_chromatin walz_myc_dox_join.bed clean.walz_U2OS_AcH4_-Dox_ChIPseq.fastq.gz.sam.processed.bam.bdg clean.walz_U2OS_AcH4_+Dox_ChIPseq.fastq.gz.sam.processed.bam.bdg
 bedmap_chromatin barfeld_myc_join.bed clean.barfeld_H3K27ac_R1881.fastq.gz.sam.processed.bam.bdg clean.barfeld_H3K27ac_R1881_Dox.fastq.gz.sam.processed.bam.bdg
-bedmap_chromatin pereira_astrocytes_ngn2_join.bed clean.pereira_astrocytes_GSM6368722_ATACseq_GFP_rep1.sam.rmdup.bam.bdg clean.pereira_astrocytes_GSM6368725_ATACseq_Ngn2_rep1.sam.rmdup.bam.bdg
-bedmap_chromatin wang_ascl1_join.bed clean.wang_ATAC_GIMEN_ASCL1_Neg_Rep1.sam.processed.bam.bdg clean.wang_ATAC_GIMEN_ASCL1_Pos_Rep1.sam.processed.bam.bdg
-bedmap_chromatin manandhar_myod1_join.bed clean.manandhar_GSM2449222_Fb_rep1_dnase*bam.bdg clean.*manandhar_GSM2449225_Fd_rep1_dnase*bam.bdg
+bedmap_chromatin pereira_astrocytes_ngn2_join.bed clean.pereira_astrocytes_GSM6368722_ATACseq_GFP.sam.rmdup.bam.bdg clean.pereira_astrocytes_GSM6368725_ATACseq_Ngn2.sam.rmdup.bam.bdg
+bedmap_chromatin wang_ascl1_join.bed clean.wang_ATAC_GIMEN_ASCL1_Neg.sam.processed.bam.bdg clean.wang_ATAC_GIMEN_ASCL1_Pos.sam.processed.bam.bdg
+bedmap_chromatin manandhar_myod1_join.bed clean.manandhar_GSM2449222_Fb_dnase*bam.bdg clean.*manandhar_GSM2449225_Fd_dnase*bam.bdg
 bedmap_chromatin li_tw2_gm_induction_join.bed clean.liu_GSM3659988_GFP-GM_K27ac_1*bam.bdg clean.li_Tw2-GM_K27ac_1.fastq.gz.sam.processed.bam.bdg
 bedmap_chromatin conerly_myf5_join.bed clean.mef_conerly_h4ac.sam.processed.bam.bdg clean.conerly_GSM1954035_H4Ac_Myf5*bam.bdg
 bedmap_chromatin conerly_myod_join.bed clean.mef_conerly_h4ac.sam.processed.bam.bdg clean.conerly_GSM1954034_H4Ac_MyoDs*bam.bdg
